9e0c84e4c608bd2a71d008dc633ee2754d758c6e
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-05-07 11:02:19 +0900

    urls static text

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path(api_name + api_version + 'user/', include('user.urls', namespace='user')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
