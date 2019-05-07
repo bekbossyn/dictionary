1f8cf6f3cad0dc764f7a26e2a568add5f4c6bf24
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-05-07 12:53:54 +0900

    fixes revert

--- word/views.py
+++ word/views.py
@@ -8,13 +8,13 @@ def test_word(request):
     return render(request, "test/test_word.html", {})
 
 
-@http.json_response()
-def test_show_word(request):
-	try:
-        word = Word.objects.latest('id')
-    except ObjectDoesNotExist:
-        return http.code_response(code=codes.BAD_REQUEST, message=messages.USER_NOT_FOUND)
-    return {
-        "user": word.json(),
-    }
+# @http.json_response()
+# def test_show_word(request):
+# 	try:
+#         word = Word.objects.latest('id')
+#     except ObjectDoesNotExist:
+#         return http.code_response(code=codes.BAD_REQUEST, message=messages.USER_NOT_FOUND)
+#     return {
+#         "user": word.json(),
+#     }
 
