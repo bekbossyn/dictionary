3837044cb18b105fad7818d421d6ec80c74779a9
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-05-07 12:50:10 +0900

    test show word

--- word/views.py
+++ word/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+from .models import Word
+
 
 # Create your views here.
 def test_word(request):
     return render(request, "test/test_word.html", {})
 
+
+@http.json_response()
+def test_show_word(request):
+	try:
+        word = Word.objects.latest('id')
+    except ObjectDoesNotExist:
+        return http.code_response(code=codes.BAD_REQUEST, message=messages.USER_NOT_FOUND)
+    return {
+        "user": word.json(),
+    }
 
