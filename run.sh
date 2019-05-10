4f5bad438d1f7cacd884c264081bd90b55193e4c
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-05-10 17:40:43 +0900

    fixes shell

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source '/home/development/env_dictionary/bin/activate'
-cd /home/development/dictionary/ && pip install -r requirements.txt
+pip install -r requirements.txt
 python ocean.py collectstatic --noinput
 python ocean.py migrate --noinput
