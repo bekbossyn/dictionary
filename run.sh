2ae8eb3f51f59019083d2479ff7bf75ab4a38f88
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-05-10 18:10:42 +0900

    fis

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+pwd
 cd /home/development/env_dictionary/bin
+pwd
 ls | grep 'myfile.py'
+pwd
 source '/home/development/env_dictionary/bin/activate'
 cd  /home/development/dictionary/ && pip install -r requirements.txt
 python ocean.py collectstatic --noinput
