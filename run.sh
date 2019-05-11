bef93e92772b5d457d21bdb3b9253747bfa1390c
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-05-11 12:52:18 +0900

    F

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-#source '/home/development/env_dictionary/bin/activate'
-source '../env_dictionary/bin/activate'
+source '/home/development/env_dictionary/bin/activate'
 pip install -r requirements.txt
 python server.py collectstatic --noinput
 python server.py migrate --noinput
