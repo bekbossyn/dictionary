315a0232f14b5791e95f6df84bf91115335e7fac
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-05-11 12:17:59 +0900

    Asdf

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!usr/bin/env bash
 source '../home/development/env_dictionary/bin/activate'
+which python
+pwd
 pip install -r requirements.txt
 python ocean.py collectstatic --noinput
 python ocean.py migrate --noinput
